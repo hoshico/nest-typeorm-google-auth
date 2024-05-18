7f0002a9c0cc37096fa261794860e59d3fd1fa0b
hoshico <melancholinista.yasu@gmail.com>
2024-05-18 13:57:07 +0900

    Remove unneccesary files

--- nest-app/src/employee-infos/employee-infos.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { EmployeeInfosController } from './employee-infos.controller';
-import { EmployeeInfosService } from './employee-infos.service';
-
-@Module({
-  controllers: [EmployeeInfosController],
-  providers: [EmployeeInfosService],
-})
-export class EmployeeInfosModule {}
