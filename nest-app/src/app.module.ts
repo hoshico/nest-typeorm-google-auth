4852c674de49f40dfb64d06399fc10e271e1ab05
hoshico <melancholinista.yasu@gmail.com>
2024-05-18 14:04:23 +0900

    removed unnecessary import

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -1,12 +1,41 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { EmployeeInfosModule } from './employee-infos/employee-infos.module';
-import { GoogleModule } from './google/google.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthMiddleware } from './middleware/auth.middleware';
 
 @Module({
-  imports: [EmployeeInfosModule, GoogleModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'production'
+          ? '.env.production'
+          : '.env.development',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        migrations: ['dist/migrations/**/*.js'],
+        entities: ['dist/entities/**/*.entity.js'],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    // 新たにmoduleを作成した場合、importsに追加
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*');
+  }
+}
