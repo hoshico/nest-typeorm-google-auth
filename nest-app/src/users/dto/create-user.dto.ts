74d29a6aa1f98792bba150582f6a6d8b6a924764
hoshico <melancholinista.yasu@gmail.com>
2024-05-18 14:15:13 +0900

    Update dto

--- nest-app/src/users/dto/create-user.dto.ts
+++ nest-app/src/users/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsString, IsEmail, IsDateString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsDateString()
+  birthDate: string;
+}
